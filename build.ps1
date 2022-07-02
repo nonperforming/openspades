49661d149d1df7a473505c0924d24a661e8d9d49
Non <43197300+nonperforming@users.noreply.github.com>
2022-07-02 12:15:07 +1200

    Update build script

--- build.ps1
+++ build.ps1
@@ -2,71 +2,134 @@
 # You may try to use it to build locally,
 # but your milage may vary.
 
-#$ErrorActionPreference = 'Stop';
+# Parameters:
+# Defaults to OpenSpades if no argument is given
+# O - OpenSpades
+# P - OpenSpades+
+# N - NucetoSpades
+# Z - ZeroSpades
+# C - Custom (provide Git url)
+
+#region AppVeyor CI Failure
+#$ErrorActionPreference = 'Stop'
 #-- Performing Test HAS_MSGHDR_FLAGS
 #-- Performing Test HAS_MSGHDR_FLAGS - Failed
 #-- Check size of socklen_t
 #-- Check size of socklen_t - failed
 #The running command stopped because the preference variable "ErrorActionPreference" or common parameter is set to Stop: CMake Warning (dev) in Resources/CMakeLists.txt:
-
-Write-Host "Build start..."
+#
+#Note: does not actually affect build, probably
+#endregion
 
 #region Windows
 function BuildWindows
 {
-  Write-Host "Building for Windows..."
-  
   # Based off of https://github.com/Conticop/OpenSpades-assets/blob/main/build-openspades.ps1
   
-  Write-Host "Setting variables"
+  Write-Host "Building for Windows..."
   $RepoRoot = "" + (Get-Location)
-  Write-Host Repository root: $RepoRoot
 
-  Write-Host "Installing dependencies using VCPkg"
-  vcpkg/bootstrap-vcpkg.bat -disableMetrics
-  vcpkg/vcpkg install "@vcpkg_x86-windows.txt"
+  try
+  {
+    Write-Host "Installing dependencies using VCPkg"
+    vcpkg/bootstrap-vcpkg.bat -disableMetrics
+    vcpkg/vcpkg install "@vcpkg_x86-windows.txt"
+  }
+  catch
+  {
+	ErrorDependencies
+  }
 
   Write-Host "Configuring build using CMake"
   try { cmake -A Win32 -D "CMAKE_BUILD_TYPE=MinSizeRel" -D "CMAKE_TOOLCHAIN_FILE=$RepoRoot/vcpkg/scripts/buildsystems/vcpkg.cmake" -D "VCPKG_TARGET_TRIPLET=x86-windows-static" "-S$RepoRoot" "-B$RepoRoot/build" }
-  catch { throw "CMake failed" }
+  catch { ErrorCMake }
   
-  Write-Host "Building OpenSpades+"
+  Write-Host "Building $OpenSpadesFlavorName"
   try { cmake --build "$RepoRoot/build" --config MinSizeRel --parallel 16 }
-  catch { throw "Build failed" } 
+  catch { throw ErrorBuild } 
   
   Write-Host "Zipping binary"
   7z a Windows.zip build/bin/MinSizeRel -r
+  
+  BuildSuccess
 }
 #endregion
 
-#region Linux
+
+
+#region Linux check distro
 function BuildLinux
 {
   Write-Host "Building for GNU/Linux..."
- 
+  
+  $Distro = lsb_release -i
+  $Distro = $Distro.substring(16)
+  
+  switch
+  {
+    <$Distro -eq Ubuntu> { BuildUbuntu; break}
+	<$Distro -eq Debian> { BuildDebian; break}
+	<$Distro -eq Arch> { BuildArch; break}	
+	<$Distro -eq Manjaro> { BuildArch; break}
+	<default> { ErrorUnknownDistro; break}
+  }
+}
+#endregion
+
+#region Debian
+function BuildDebian
+{
+  
+}
+#endregion
+
+#region Ubuntu
+function BuildUbuntu
+{
+  Write-Host "Building for Ubuntu..."
+  
   Write-Host "Updating packages and installing dependencies using apt-get"
-  sudo apt-get update
-  sudo apt-get install ninja-build pkg-config libglew-dev libcurl3-openssl-dev libsdl2-dev libsdl2-image-dev libalut-dev xdg-utils libfreetype6-dev libopus-dev libopusfile-dev cmake imagemagick libjpeg-dev libxinerama-dev libxft-dev -y
-  sudo apt-get upgrade
+  try
+  {
+    sudo apt-get update
+    sudo apt-get install ninja-build pkg-config libglew-dev libcurl3-openssl-dev libsdl2-dev libsdl2-image-dev libalut-dev xdg-utils libfreetype6-dev libopus-dev libopusfile-dev cmake imagemagick libjpeg-dev libxinerama-dev libxft-dev -y
+    sudo apt-get upgrade -y
+  }
+  catch
+  {
+    Write-Host "Installing and updating dependencies failed"
+	ErrorDependencies
+  }
   
   Write-Host "Creating build directory"
   mkdir build
   Push-Location build
   
   Write-Host "Configuring build using CMake"
-  try { cmake .. -G Ninja -DCMAKE_BUILD_TYPE=MinSizeRel}
-  catch { throw "CMake failed" }
+  try { cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="-O2" }
+  catch { ErrorCMake }
   
   Write-Host "Building using Ninja"
   try { make -j 16 }
-  catch { throw "Build failed" }
+  catch { ErrorBuild }
   
   Write-Host "Zipping binary"
   7z a Linux.zip bin -r
   Write-Host "Zipping resources"
   7z a Resources.zip Resources -r
+  
+  BuildSuccess
 }
 #endregion
+
+#region Arch/Manjaro
+function BuildArch
+{
+  
+}
+#endregion
+
+
 
 #region Mac OS
 function BuildMacOS
@@ -92,10 +155,108 @@ function BuildMacOS
   
   Write-Host "Zipping binary"
   7z a MacOS.zip bin/OpenSpades.app -r
+  
+  BuildSuccess
 }
 #endregion
 
+
+
+#region Success
+function BuildSuccess
+{
+  Write-Host "Build finished"
+  exit 0
+}
+#endregion
+
+#region Errors
+function ErrorDependencies
+{
+  throw "Failed updating or installing dependencies"
+  exit 417
+}
+
+function ErrorUnknown
+{
+  throw "Unknown OS - cannot continue"
+  exit 404
+}
+
+function ErrorUnknownDistro
+{
+  throw "Unknown GNU/Linux Distro ($Distro) - cannot continue"
+  exit 404
+}
+
+function ErrorCMake
+{
+  throw "Error generating build files with CMake"
+  exit 400
+}
+
+function ErrorBuild
+{
+  throw "Error building"
+  exit 400
+}
+#endregion
+
+#region Warnings
+function WarningPullFailed
+{
+  #TODO: throw or write-error
+  #or write-host???????
+  #how does powershell work anyways????
+  Write-Host "Failed pulling latest changes from Git repository\nProceeding anyways"
+}
+#endregion
+
+
 #region Pick the correct build script to run
+param
+(
+  [Parameter()]
+  [String] $OpenSpadesFlavor = P,
+  [String] $URL = "https://github.com/nonperforming/openspadesplus.git",
+  [String] $DirectoryName = "OpenSpadesPlus"
+)
+
+$OpenSpadesFlavorName
+
+switch
+{
+  <$OpenSpadesFlavor -eq C> { Write-Host "Building OpenSpades (custom)"; $OpenSpadesFlavorName = "Custom"; break;}
+  <$OpenSpadesFlavor -eq O> { Write-Host "Building OpenSpades"; $URL = "https://github.com/yvt/openspades.git"; $OpenSpadesFlavorName = "OpenSpades"; $DirectoryName = "OpenSpades"; break;}
+  <$OpenSpadesFlavor -eq P> { Write-Host "Building OpenSpades+"; $URL = "https://github.com/nonperforming/openspadesplus.git"; $OpenSpadesFlavorName = "OpenSpadesPlus"; $DirectoryName = "OpenSpadesPlus"; break;}
+}
+
+Write-Host "Cloning/pulling changes from Git repository [$URL] - ($OpenSpadesFlavorName)"
+
+if (Test-Path "$DirectoryName")
+{
+  Write-Host "Repository already exists on drive!"
+  
+  Push-Location $DirectoryName
+  
+  try
+  {
+    git pull
+  }
+  catch
+  {
+    WarningPullFailed
+  }
+
+}
+else
+{
+  Write-Host "Repository does not exist on drive! Cloning repository..."
+  
+  git clone $URL $DirectoryName;
+  Push-Location $DirectoryName
+}
+
 if ($isWindows)
 {
   BuildWindows
@@ -110,9 +271,6 @@ elseif ($isMacOS)
 }
 else
 {
-  # Unknown OS
-  throw "Unknown OS - cannot continue"
+  ErrorUnknown
 }
 #endregion
-
-Write-Host "Build success"
